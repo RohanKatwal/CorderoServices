f4d8ad2319496e5b6537bae12d0da6e619224699
Ronish Katuwal <ronishkatuwal00@gmail.com>
2025-11-24 22:00:05 +0545

    converted server page to client

---
+++ src/app/(root)/inmobiliaria/InmobiliariaClient.tsx
@@ -0,0 +1,50 @@
+"use client";
+import Contact from "@/components/shared/contact/Contact";
+import HeroSection from "@/components/shared/HeroSection";
+import { VideoType } from "@/components/shared/modal/VideoModal";
+import Navbar from "@/components/shared/Navbar";
+import Stats from "@/components/shared/Stats";
+import Video from "@/components/shared/Video";
+import { getVideos } from "@/lib/actions/firebaseVideos";
+import { useEffect, useState } from "react";
+
+const InmobiliariaClient = () => {
+  const [videos, setVideos] = useState<VideoType[] | null>(null);
+  useEffect(() => {
+    getVideos().then(setVideos);
+  }, []);
+  return (
+    <>
+      <Navbar pageName="inmobiliaria" />
+      <HeroSection pageName="inmobiliaria" />
+      <Stats pageName="inmobiliaria" />
+      <section className="videos-wrapper container">
+        <div className="heading">
+          <h1>
+            Nuestros Destacados <span className="highlight">Inmobiliarios</span>
+          </h1>
+          <p>
+            Explora nuestros últimos destacados de propiedades y proyectos a
+            través de video.
+          </p>
+        </div>
+        <ul className="videos">
+          {videos &&
+            videos.map((video) => (
+              <Video
+                key={video.id}
+                id={video.id}
+                title={video.title}
+                description={video.description}
+                location={video.location}
+                url={video.url}
+              />
+            ))}
+        </ul>
+      </section>
+      <Contact />
+    </>
+  );
+};
+
+export default InmobiliariaClient;
--- src/app/(root)/inmobiliaria/page.tsx
+++ src/app/(root)/inmobiliaria/page.tsx
@@ -1,9 +1,4 @@
-import Contact from "@/components/shared/contact/Contact";
-import HeroSection from "@/components/shared/HeroSection";
-import Navbar from "@/components/shared/Navbar";
-import Stats from "@/components/shared/Stats";
-import Video from "@/components/shared/Video";
-import { getVideos } from "@/lib/actions/firebaseVideos";
+import InmobiliariaClient from "./InmobiliariaClient";
 
 export const metadata = {
   title: "Inmobiliaria",
@@ -12,39 +7,10 @@ export const metadata = {
   },
 };
 
-const InmobiliariaPage = async () => {
-  const videos = await getVideos();
+const InmobiliariaPage = () => {
   return (
-    <>
-      <Navbar pageName="inmobiliaria" />
-      <HeroSection pageName="inmobiliaria" />
-      <Stats pageName="inmobiliaria" />
-      <section className="videos-wrapper container">
-        <div className="heading">
-          <h1>
-            Nuestros Destacados <span className="highlight">Inmobiliarios</span>
-          </h1>
-          <p>
-            Explora nuestros últimos destacados de propiedades y proyectos a
-            través de video.
-          </p>
-        </div>
-        <ul className="videos">
-          {videos.map((video) => (
-            <Video
-              key={video.id}
-              id={video.id}
-              title={video.title}
-              description={video.description}
-              location={video.location}
-              url={video.url}
-            />
-          ))}
-        </ul>
-      </section>
-      <Contact />
-    </>
+    <InmobiliariaClient />
   );
-};
-
+}
+ 
 export default InmobiliariaPage;
